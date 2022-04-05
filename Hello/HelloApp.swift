0b6eabb923324970e1a29076b5209a94ec403e92
Mako <sakakibaramako@gmail.com>
2022-04-05 03:31:45 -0700

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by stlp on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
